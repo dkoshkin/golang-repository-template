947e0d2157be50ed67ad8875a05b6c003d6523f5
Dimitri Koshkin <dimitri.koshkin@gmail.com>
2025-08-11 21:24:26 -0700

    build: Switch to Devbox (#3)

--- make/goreleaser.mk
+++ make/goreleaser.mk
@@ -1,10 +1,8 @@
-# Copyright 2023 Dimitri Koshkin. All rights reserved.
+# Copyright 2025 Dimitri Koshkin. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 
-export GOMODULENAME ?= $(shell go list -m)
-
 GORELEASER_PARALLELISM ?= $(shell nproc --ignore=1)
-GORELEASER_DEBUG ?= false
+GORELEASER_VERBOSE ?= false
 
 ifndef GORELEASER_CURRENT_TAG
 export GORELEASER_CURRENT_TAG=$(GIT_TAG)
@@ -12,19 +10,19 @@ endif
 
 .PHONY: build-snapshot
 build-snapshot: ## Builds a snapshot with goreleaser
-build-snapshot: install-tool.goreleaser install-tool.golang ; $(info $(M) building snapshot $*)
-	goreleaser --debug=$(GORELEASER_DEBUG) \
+build-snapshot: go-generate ; $(info $(M) building snapshot $*)
+	goreleaser --verbose=$(GORELEASER_VERBOSE) \
 		build \
 		--snapshot \
 		--clean \
 		--parallelism=$(GORELEASER_PARALLELISM) \
-		$(if $(BUILD_ALL),,--single-target) \
-		--skip-post-hooks
+		--config=<(env GOOS=$(shell go env GOOS) gojq --yaml-input --yaml-output '.builds[0].goos |= (. + [env.GOOS] | unique)' .goreleaser.yml) \
+		$(if $(BUILD_ALL),,--single-target)
 
 .PHONY: release
 release: ## Builds a release with goreleaser
-release: install-tool.goreleaser install-tool.golang ; $(info $(M) building release $*)
-	goreleaser --debug=$(GORELEASER_DEBUG) \
+release: go-generate ; $(info $(M) building release $*)
+	goreleaser --verbose=$(GORELEASER_VERBOSE) \
 		release \
 		--clean \
 		--parallelism=$(GORELEASER_PARALLELISM) \
@@ -33,11 +31,14 @@ release: install-tool.goreleaser install-tool.golang ; $(info $(M) building rele
 
 .PHONY: release-snapshot
 release-snapshot: ## Builds a snapshot release with goreleaser
-release-snapshot: install-tool.goreleaser install-tool.golang ; $(info $(M) building snapshot release $*)
-	goreleaser --debug=$(GORELEASER_DEBUG) \
+release-snapshot: go-generate ; $(info $(M) building snapshot release $*)
+	goreleaser --verbose=$(GORELEASER_VERBOSE) \
 		release \
 		--snapshot \
-		--skip-publish \
 		--clean \
 		--parallelism=$(GORELEASER_PARALLELISM) \
 		--timeout=60m
+
+.PHONY: list-releases
+list-releases: ## List releases from GitHub
+	gh release list --json tagName | gojq -r .[].tagName
