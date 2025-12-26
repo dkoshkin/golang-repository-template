137fbf1dade403538052f7101ec004d48483eee8
Dimitri Koshkin <dimitri.koshkin@gmail.com>
2025-12-25 22:58:51 -0500

    fix: sample main.go to work with helm

--- cmd/golang-repository-template/main.go
+++ cmd/golang-repository-template/main.go
@@ -3,8 +3,56 @@
 
 package main
 
-import "fmt"
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
 
 func main() {
-	fmt.Println("golang-repository-template is alive")
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, _ *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		_, _ = w.Write([]byte("ok"))
+	})
+
+	server := &http.Server{
+		Addr:              ":" + port,
+		Handler:           mux,
+		ReadHeaderTimeout: 5 * time.Second,
+	}
+
+	errs := make(chan error, 1)
+	go func() {
+		log.Printf("listening on :%s", port)
+		errs <- server.ListenAndServe()
+	}()
+
+	sigc := make(chan os.Signal, 1)
+	signal.Notify(sigc, syscall.SIGINT, syscall.SIGTERM)
+
+	select {
+	case sig := <-sigc:
+		log.Printf("shutdown signal: %s", sig)
+	case err := <-errs:
+		if err != nil && err != http.ErrServerClosed {
+			log.Fatalf("server error: %v", err)
+		}
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		log.Printf("graceful shutdown failed: %v", err)
+	}
 }
