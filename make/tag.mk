3445efd13eb3ed653e6295bb12a8b6f145673769
Dimitri Koshkin <dimitri.koshkin@gmail.com>
2026-01-15 21:18:39 -0800

    fix: tag additional modules on release

--- make/tag.mk
+++ make/tag.mk
@@ -11,3 +11,13 @@ endif
 		git tag -s "$(module)$(NEW_GIT_TAG)" -a -m "$(module)$(NEW_GIT_TAG)";\
 	)
 	git tag -s "$(NEW_GIT_TAG)" -a -m "$(NEW_GIT_TAG)"
+
+.PHONY: tag-modules
+tag-modules:
+ifndef NEW_GIT_TAG
+	$(error Please specify git tag to create via NEW_GIT_TAG env var or make variable)
+endif
+	$(foreach module,\
+		$(dir $(GO_SUBMODULES_NO_DOCS)),\
+		git tag -s "$(module)$(NEW_GIT_TAG)" -a -m "$(module)$(NEW_GIT_TAG)";\
+	)
